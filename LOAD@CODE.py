547ee519326fd8f7a7ffa663bb67acaa24bd2db2
Tanoi-Gamers-673-x <62834829+Tanoi-Gamers-673-x@users.noreply.github.com>
2020-04-18 21:14:58 +0700

    Add files via upload

---
+++ LOAD@CODE.py
@@ -0,0 +1,130 @@
+#Python Programing By Tanoi Ganers
+#Youtube Chanel Tanoi Gamers
+#uname on Git hub >> Tanoi-Gamers-673-x
+
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+#CodeLoad
+os.system("clear")
+print
+os.system("figlet LOAD@CODE | lolcat")
+print "\033[1m\033[35mPython Programing By Tanoi Gamers"
+print "\033[0m"
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+
+A = raw_input("Please enter to Continue..")
+#Profile
+os.system("pkg upgarde -y")
+os.system("pkg update -y")
+os.system("pkg install python2 -y ")
+os.system("pkg install python -y ")
+os.system("pkg install git -y")
+os.system("pkg install sl-y")
+os.system("pkg install sh -y")
+os.system("pkg install nano -y")
+os.system("pkg install figlet -y")
+os.system("clear")
+os.system("sl")
+
+E = raw_input("Please [Enter] to continued..")
+os.system("clear")
+os.system("figlet LOAD@CODE | lolcat")
+print "Author   : Tanoi Gamers"
+print "Group    : -                  "
+print "Codename : LC-001             "
+print "Version  : 0.1                "
+print
+print "\033[1m\033[35m The Ceartor Is Tanoi Gamers"
+print "\033[0m"
+print
+
+print "          [01]>[Loadcode]          "
+print "          [02]>[TOOL673X]          "
+print "          [00>[Exit To Home]       "
+options = raw_input("L@C>>")
+
+if options == "1" or options == "01":
+ os.system("clear")
+ os.sysetn("pkg install wget -y")
+ os.system("pkg install curl -y")
+ os.system("pkg upgarde -y")
+ os.system("pkg install python -y")
+ os.system("pkg install python2 -y")
+ os.system("pkg install git -y")
+ os.system("pkg install ruby -y ")
+ os.system("pkg install -y pv neofetch fish man libcaca curl cmatrix cowsay figlet toilet")
+ os.system("gem install lolcat -y")
+ os.system("pkg install figlet")
+ os.system ("pkg install python")
+ os.system("pkg install python2")
+ os.system("pkg install python3")
+ os.system("pkg upgrade")
+ os.system("pkg --upgarde")
+ os.system("pkg --upgardable")
+ os.system("dpkg --upgardable")
+ os.system("pkg install update -y")
+ os.system("pkg install npm")
+ os.system("pkg install nano")
+ os.system("pkg install irssi")
+ os.system("pkg install")
+ os.system("pkg install cmatrix -y")
+ os.system("pkg install toilet -y")
+ os.system("pkg install figlet -y")
+ os.system("pkg install cowsay -y")
+ os.system("pkg install -y pv neofetch fish man libcaca curl cmatrix cowsay figlet toilet")
+ os.system("pkg install wordlist -y")
+ os.system("pkg install repo-root -y")
+ os.system("pkg install pip -y")
+ os.system("pkg install pip2 -y")
+ os.system("pkg install pornhud -y")
+ os.system("pkg install bash -y")
+ os.system("pkg install tsu -y")
+ os.system("pkg install pup -y")
+ os.system("pkg install php -y")
+ os.system("pkg install openssh")
+ os.system("pkg install sl")
+ os.system("pkg install sh")
+ os.system("pkg install ")
+ os.system("clear")
+ os.system("cd $HOME")
+ os.system("cd LOAD@CODE")
+ os.system("python2 LOAD@CODE.py")
+
+elif options == "02" or options == "2":
+ os.system("clear")
+ print "\033[1m\033[35m TOOL673X "
+ os.system("git clone https://github.com/Tanoi-Gamers-673-x/TOOL673X")
+ os.system("mv TOOL673X")
+ os.system("cd $HOME")
+ os.system("cd TOOL673X")
+ os.system("python2 Tools-Hack-673x.py")
+
+elif options == "00" or options == "0":
+ os.system("clear")
+ os.system("figlet LOAD@CODE | lolcat ")
+ os.system("cd")
+ os.system("neofetch")
+ print "\033[1m\033[35m Python Programming By Tanoi Gamers"
+
+else:
+     print "\nERROR: Wrong Input"
